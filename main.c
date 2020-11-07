0d0799b4c01aaaca29a0108c3c720946f45d8e32
dvirbz <58605853+dvirbz@users.noreply.github.com>
2020-11-07 13:09:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
-#define _CRT_SECURE_NO_WARNINGS 1
-
-
 #include "father_utility.h"
 
 int main(int argc, const char* argv[])
@@ -15,12 +14,12 @@ int main(int argc, const char* argv[])
 		return(-1);
 	}
 
-	FILE* p_file;
+	FILE* p_file;//check
 	errno_t err;
 
 	// Open for read (will fail if file  in argv[1] does not exist)
 	//-----------------
-	err = fopen_s(&p_file, "C:\test.txt", "r");
+	err = fopen_s(&p_file, argv[1], "r");
 	if (err)
 	{
 		printf("could't open file. ABORT");
@@ -28,8 +27,8 @@ int main(int argc, const char* argv[])
 	}
 	//------------------
 
-	int forest_size = 0, *p_forest_size = &forest_size;
-	int generations = 0, *p_generations = &generations;
+	int forest_size = 0, * p_forest_size = &forest_size;
+	int generations = 0, * p_generations = &generations;
 
 	//read first two lines in file and obtain parameters of forest.
 	get_forest_parameters(p_file, p_forest_size, p_generations);
@@ -38,12 +37,13 @@ int main(int argc, const char* argv[])
 	/*notice that forest_size is in fact the number of columns in the forest*/
 
 	int cur_max = (forest_size + 1) * 2;
+	int line_size = (forest_size + 1) * 2;
 
-	char* line = (char*)malloc(sizeof(char) * cur_max);
+	char* line = (char*)malloc(sizeof(char) * line_size);
 	char* forest = (char*)malloc(sizeof(char) * cur_max);
 	char* tmp;
 
-	int line_size = cur_max;
+	
 	if (NULL == line || NULL == forest)
 	{
 		printf("FATAL ERROR: memory alloaction failed. ABORT");
@@ -56,7 +56,7 @@ int main(int argc, const char* argv[])
 		rsize_t strmax = sizeof line;
 		char* token;
 		char* next_token;
-		token = strtok_s(line, &strmax, ",", &next_token);
+		token = strtok_s(line, ",", &next_token);
 		while (token != NULL)
 		{
 			if (NULL != forest)
@@ -78,18 +78,17 @@ int main(int argc, const char* argv[])
 			}
 			else
 				return -1;
-			
+
 
 			/*create string of letters defining forest*/
-			strcat(forest, token);
-
+			strcat(forest, token);			
 			/* walk through other tokens */
-			token = strtok_s(NULL, &strmax, ",", &next_token);
+			token = strtok_s(NULL, ",", &next_token);
 		}
 	}
 	/*free space allocated for string line*/
 	free(line);
-	fclose(argv[1]);
+	fclose(p_file);
 
 	/*open outputfile for writing*/
 	/*FILE* out_file;
@@ -100,10 +99,12 @@ int main(int argc, const char* argv[])
 		return -1;
 	}
 	fputs(forest, out_file);*/
+	printf("%s", forest);
 	free(forest);
 
 	//close files
 	//fclose(out_file);
-	
+
 	return(0);
 }
+#endif
