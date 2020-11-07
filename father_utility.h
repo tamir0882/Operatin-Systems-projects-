072516fe8297609d66f04f45c06e7123b91a90c5
tamir0882 <58632401+tamir0882@users.noreply.github.com>
2020-11-07 11:32:33 +0200

    Add files via upload

---
+++ father_utility.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef __father_utility_H__
+#define __father_utility_H__
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE_OF_STR 20
+
+void get_forest_parameters(FILE* file, int* size, int* generations);
+
+//int get_forest_line(FILE* file, const int size, char* str);
+
+
+#endif // __father_utility_H__
+
--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <errno.h>
-
+#include <string.h>
+#include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS 1
 
 
 #include "father_utility.h"
 
-#define _CRT
 int main(int argc, const char* argv[])
 {
 	if (2 != argc)
@@ -20,14 +20,13 @@ int main(int argc, const char* argv[])
 
 	// Open for read (will fail if file  in argv[1] does not exist)
 	//-----------------
-	err = fopen_s(&p_file, argv[1], "r");
+	err = fopen_s(&p_file, "C:\test.txt", "r");
 	if (err)
 	{
 		printf("could't open file. ABORT");
 		return -1;
 	}
 	//------------------
-
 
 	int forest_size = 0, *p_forest_size = &forest_size;
 	int generations = 0, *p_generations = &generations;
@@ -41,6 +40,7 @@ int main(int argc, const char* argv[])
 	int cur_max = (forest_size + 1) * 2;
 
 	char* line = (char*)malloc(sizeof(char) * cur_max);
+	line[cur_max - 1] = '\0';
 	char* forest = (char*)malloc(sizeof(char) * cur_max);
 	if (NULL == line || NULL == forest)
 	{
@@ -55,17 +55,22 @@ int main(int argc, const char* argv[])
 		token = strtok(line, ",");
 		while (token != NULL)
 		{
-			if (strlen(forest) == cur_max)
+			if (strlen(forest) >= cur_max - 1)
 			{
 				cur_max *= 2;
 				forest = realloc(forest, cur_max);
+				if (NULL == forest)
+				{
+					printf("memory reallocation failed. ABORT");
+					return -1;
+				}
 			}
 
 			/*create string of letters defining forest*/
 			strcat(forest, token);
 
 			/* walk through other tokens */
-			token = strtok(NULL, ',');
+			token = strtok(NULL, ",");
 		}
 	}
 	/*free space allocated for string line*/
