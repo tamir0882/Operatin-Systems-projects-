b63ef09937d44b51cc94572e937889c0e86fac4d
tamir0882 <58632401+tamir0882@users.noreply.github.com>
2020-11-07 10:39:39 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <errno.h>
 
+#define _CRT_SECURE_NO_WARNINGS 1
+
 
 #include "father_utility.h"
 
-
+#define _CRT
 int main(int argc, const char* argv[])
 {
 	if (2 != argc)
@@ -15,86 +17,74 @@ int main(int argc, const char* argv[])
 
 	FILE* p_file;
 	errno_t err;
+
 	// Open for read (will fail if file  in argv[1] does not exist)
+	//-----------------
 	err = fopen_s(&p_file, argv[1], "r");
-	if (err == 0)
+	if (err)
 	{
-		printf("The file 'forest.txt' was opened\n");
+		printf("could't open file. ABORT");
+		return -1;
 	}
-	else
-	{
-		printf("The file 'forest.txt' was not opened\n");
-		return(-1);
-	}
+	//------------------
 
 
-	
 	int forest_size = 0, *p_forest_size = &forest_size;
 	int generations = 0, *p_generations = &generations;
 
 	//read first two lines in file and obtain parameters of forest.
 	get_forest_parameters(p_file, p_forest_size, p_generations);
 
-	int CUR_MAX = (forest_size + 1) * 2;
-	
-	char* line = (char*)malloc(sizeof(char) * CUR_MAX); // allocate memory for line.
-	
-	char* line_without_comma;
 
+	/*notice that forest_size is in fact the number of columns in the forest*/
 
-	//obtain first line and its length(return value of the function is the length of the line)
-	int length = get_forest_line(p_file, CUR_MAX, line);
+	int cur_max = (forest_size + 1) * 2;
 
-
-	// allocate memory for forest, which is meant to be the full line of letters defining the forest.
-	char* forest = (char*)malloc(sizeof(char) * length); 
-	int i = 0;
+	char* line = (char*)malloc(sizeof(char) * cur_max);
+	char* forest = (char*)malloc(sizeof(char) * cur_max);
+	if (NULL == line || NULL == forest)
+	{
+		printf("FATAL ERROR: memory alloaction failed. ABORT");
+		return -1;
+	}
 
 	while (!feof(p_file))
 	{
-		 //return value of this function is the length of the obtained line.
-
-		/*up to this point we read one line from the file, which contains commas in between the letters.*/
-		/*now we would like to remove the commas and leave only the letters in the line*/
-
+		fgets(line, cur_max, p_file);
 		char* token;
-		line_without_comma = (char*)malloc(sizeof(char) * length);
+		token = strtok(line, ",");
+		while (token != NULL)
+		{
+			if (strlen(forest) == cur_max)
+			{
+				cur_max *= 2;
+				forest = realloc(forest, cur_max);
+			}
 
-		/* get the first token */
-		token = strtok(line, ',');
-		strcpy(line_without_comma, token);
-		
-		while (token != NULL) 
-		{	
+			/*create string of letters defining forest*/
+			strcat(forest, token);
+
 			/* walk through other tokens */
 			token = strtok(NULL, ',');
-			strcat(line_without_comma, token);
 		}
-
-
-		if (0 == i)
-		{
-			strcpy(forest, line_without_comma);
-			i++;
-		}
-		int length = get_forest_line(p_file, CUR_MAX, line);
-
-		forest = realloc(forest, (strlen(forest) + length) * sizeof(char));
-
-		strcat(forest, line);
-
-
-
-
 	}
-	/*free spac allocated*/
+	/*free space allocated for string line*/
 	free(line);
-	free(line_without_comma);
+	fclose(argv[1]);
 
-
+	/*open outputfile for writing*/
+	FILE* out_file;
+	err = fopen_s(&out_file, "output", "w");
+	if (err)
+	{
+		printf("could't open file. ABORT");
+		return -1;
+	}
+	fputs(forest, out_file);
+	free(forest);
 
 	//close files
-	fclose(argv[1]);
+	fclose(out_file);
 	
 	return(0);
 }
